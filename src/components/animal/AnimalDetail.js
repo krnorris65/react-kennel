6c88ea3e006c04820718fddf4e2d9595418e6c44
Kristen Norris <krnorris65@gmail.com>
2019-11-21 13:35:03 -0600

    refactored AnimalDetail

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -4,25 +4,23 @@ import './AnimalDetail.css'
 
 const AnimalDetail = props => {
 
-    const [currentAnimal, setCurrentAnimal] = useState({})
-    const [loadingStatus, setLoadingStatus] = useState(true)
+    const [currentAnimal, setCurrentAnimal] = useState({loadingStatus: true })
 
     const getCurrentAnimal = () => {
         //get(id) from AnimalManager and hang on to the data; put it into state
         AnimalManager.get(props.animalId)
             .then((animal) => {
-                setCurrentAnimal(animal)
-                setLoadingStatus(false)
+                setCurrentAnimal({ name: animal.name, breed: animal.breed, loadingStatus: false })
             });
-    }
-
-    const handleDelete = () => {
-        setLoadingStatus(true)
-        AnimalManager.delete(props.animalId)
+        }
+        
+        const handleDelete = () => {
+            setCurrentAnimal({ loadingStatus: true })
+            AnimalManager.delete(props.animalId)
             .then(() => props.history.push("/animals"))
-    }
-
-    useEffect(getCurrentAnimal, [])
+        }
+        
+        useEffect(getCurrentAnimal, [])
 
     return (
         <div className="card">
@@ -32,7 +30,7 @@ const AnimalDetail = props => {
                 </picture>
                 <h3>Name: <span style={{ color: 'darkslategrey' }}>{currentAnimal.name}</span></h3>
                 <p>Breed: {currentAnimal.breed}</p>
-                <button type="button" disabled={loadingStatus} onClick={handleDelete}>Discharge</button>
+                <button type="button" disabled={currentAnimal.loadingStatus} onClick={handleDelete}>Discharge</button>
 
             </div>
         </div>
