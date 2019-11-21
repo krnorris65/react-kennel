b0edc8e5991aca9e83adaf9e780ef837703fbd89
Kristen Norris <krnorris65@gmail.com>
2019-11-21 11:26:56 -0600

    hookified AnimalCard

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,5 +1,5 @@
 import { Route } from 'react-router-dom'
-import React, { Component } from 'react'
+import React from 'react'
 import Home from './home/Home'
 import AnimalList from './animal/AnimalList'
 import AnimalDetail from './animal/AnimalDetail'
--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from "react-router-dom"
 import './Animal.css';
 
-class AnimalCard extends Component {
-  render() {
+const AnimalCard = props => {
     return (
       <div className="card">
         <div className="card-content">
           <picture>
             <img src={require('./dog.svg')} alt="My Dog" />
           </picture>
-          <h3>Name: <span className="card-petname">{this.props.animal.name}</span></h3>
-          <p>Breed: {this.props.animal.breed}</p>
-          <Link to={`/animals/${this.props.animal.id}`}><button>Details</button></Link>
-          <button type="button" onClick={() => this.props.deleteAnimal(this.props.animal.id)}>Discharge</button>
+          <h3>Name: <span className="card-petname">{props.animal.name}</span></h3>
+          <p>Breed: {props.animal.breed}</p>
+          <Link to={`/animals/${props.animal.id}`}><button>Details</button></Link>
+          <button type="button" onClick={() => props.deleteAnimal(props.animal.id)}>Discharge</button>
         </div>
       </div>
     );
-  }
 }
 
 export default AnimalCard;
